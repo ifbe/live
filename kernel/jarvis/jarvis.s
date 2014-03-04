ea23a781c07059e449b369e062a59193df66cd50
ifbe <ifbe@live.cn>
2014-03-04 19:17:47 +0800

    修改：      kernel/jarvis/jarvis.s

--- kernel/jarvis/jarvis.s
+++ kernel/jarvis/jarvis.s
@@ -27,14 +27,15 @@ prepare:
     mov ecx,0xfe
     rep stosq
 
-    jmp screen
+    call ramdump
 ;_______________________________________
 
 
 
 
-;_________________main.part0(who wake me up)____________________
+;_____________________________________
 forever:
+
 hlt			;	sleep
 
 cmp byte [0xff0],0	;	(keyboard interrupt)?
@@ -50,16 +51,9 @@ dec eax			;			pointer-1
 mov al,[eax]		;			al=[pointer]
 
 
-;_________________________________
-
-
-
-;______________main.part2(what do you want)____________________
 switchkey:
-
-;specialkey........	;			(al)?
-			;			{
-cmp al,0x3b
+			;			(al)?
+cmp al,0x3b		;			{
 je f1			;			0x3b:f1
 cmp al,0x3c
 je f2			;			0x3c:f2
@@ -69,10 +63,10 @@ cmp al,0x3e
 je f4			;			0x3e:f4
 cmp al,0x01
 je esc			;			0x01:esc
-cmp al,0x1d
-je ctrl			;			0x1d:ctrl
-cmp al,0x38
-je alt			;			0x38:alt
+;cmp al,0x1d
+;je ctrl			;			0x1d:ctrl
+;cmp al,0x38
+;je alt			;			0x38:alt
 ;cmp al,0x3a
 ;je capslock		;			0x38:capslock
 ;cmp al,0x2a
@@ -82,24 +76,112 @@ je alt			;			0x38:alt
 ;cmp al,0x5b
 ;je super		;			0x0f:super
 
-nextswitch:
-jmp consoleswitch	;			default:throw it to others
-;jmp f1switch
+decide:
+jmp function1
 			;			}
-;____________________________________________________________
+			;		}
+			;	}
+;_______________________________
 
 
 
 
-;_______________main.part3(print to screen)_______________________
-screen:
+;_________________________
+f1:
+    mov dword [decide+1],function1-(decide+5)		;selfmodify
+    jmp function1
+f2:
+    mov dword [decide+1],function2-(decide+5)		;selfmodify
+    jmp function2
+f3:
+    mov dword [decide+1],function3-(decide+5)		;selfmodify
+    jmp function3
+f4:
+    mov dword [decide+1],function4-(decide+5)		;selfmodify
+    jmp function4
 
-background:
-    call ramdump	;		bottom layer
-foreground:
-    call console	;		top layer
 
-writetoscreen:
+esc:
+turnoff:
+mov dx,[0x4fc]
+mov ax,[0x4fe]
+or ax,0x2000
+out dx,ax
+
+;__________________________
+
+
+
+
+;进:esi,edi
+;出:esi,edi
+;用:
+;变:
+;______________________________
+char:
+    push rbx
+    push rcx
+
+    movzx eax,al
+    shl ax,4
+    lea esi,[eax+0xb000]
+
+    mov ecx,16           ;16行
+.yihang:
+    xor rax,rax
+    lodsb
+    push rcx
+        mov cl,8
+      .yidian:
+        shl al,1
+        jnc .bg
+        mov edx,[frontcolor]
+        mov [edi],edx
+        jmp .pointdone
+      .bg:
+        mov edx,[backcolor]
+        cmp edx,0xffffffff
+        je .pointdone
+        mov dword [edi],edx
+      .pointdone:
+        add edi,4
+        loop .yidian
+    sub edi,32           ;每个字的行头
+    add edi,1024*4            ;下1行
+    pop rcx
+    loop .yihang
+
+    add edi,32            ;下个字的行头
+    sub edi,4*1024*16            ;上16行;现在edi=下个字开头
+
+    mov eax,edi
+    mov ebx,0x100000        ;ebx=vesabase
+    ;mov ebx,[0x3028]        ;ebx=vesabase
+    sub eax,ebx             ;eax=相对距离
+
+.modfour:
+    and eax,0x00000fff           ;mod4096
+    cmp eax,0
+    jne .nochange
+
+.change:
+    add edi,4*1024*16
+    sub edi,1024*4
+
+.nochange:
+    pop rcx
+    pop rbx
+
+    ret              ;edi指向下一个字
+;______________________________________
+backcolor:dd 0
+frontcolor:dd 0xffffffff
+
+
+
+
+;____________________________________
+writescreen:
     mov esi,0x100000
     mov edi,[0x3028]
     mov bl,[0x3019]
@@ -113,76 +195,21 @@ writetoscreen:
     add edi,ebx
     loop .continue
 
-jmp forever
-			;		}
-			;	}
-;________________________________________
+ret
+;_________________________________
 
 
 
 
-;______________________________________________________________________
-f1:
-    mov dword [background+1],ramdump-(background+5)              ;selfmodify
-    ;mov dword [nextswitch+1],f1switch-(nextswitch+5)              ;selfmodify
-    jmp screen
-f2:
-    mov dword [background+1],picture-(background+5)              ;selfmodify
-    ;mov dword [nextswitch+1],f2switch-(nextswitch+5)              ;selfmodify
-    jmp screen
-f3:
-    jmp screen
-f4:
-    jmp screen
-
-
-ctrl:
-inc byte [ctrlkey]
-test byte [ctrlkey],1
-jnz .off
-    .on:
-    mov dword [nextswitch+1],consoleswitch-(nextswitch+5)        ;selfmodify
-    mov dword [foreground+1],console-(foreground+5)              ;selfmodify
-    jmp screen
-    .off:
-    mov dword [nextswitch+1],f1switch-(nextswitch+5)              ;selfmodify
-    mov dword [foreground+1],donothing-(foreground+5)            ;selfmodify
-    jmp screen
-
-
-alt:
-inc byte [altkey]
-test byte [altkey],1
-jnz .anscii
-    .hex:
-    mov dword [dumpwhat-4],dumphex-dumpwhat                      ;selfmodify
-    jmp screen
-    .anscii:
-    mov dword [dumpwhat-4],dumpanscii-dumpwhat                   ;selfmodify
-    jmp screen
-
-
-esc:
-turnoff:
-mov dx,[0x4fc]
-mov ax,[0x4fe]
-or ax,0x2000
-out dx,ax
-
-;_______________________________________________________________________________
-esckey db 0
-commandkey db 0
-tabkey db 0
-capslockkey db 0
-shiftkey db 0
-ctrlkey db 0
-altkey db 0
-superkey db 0
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>-----F1-----<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 
 
-;_______________________________________________________________________________
-f1switch:
+
+;___________________________
+function1:
+cmp al,0x39
+je f1space
 cmp al,0x4b
 je f1left
 cmp al,0x4d
@@ -191,10 +218,10 @@ cmp al,0x48
 je f1up
 cmp al,0x50
 je f1down
-cmp al,0x39
-je f1space
+cmp al,0x38
+je f1alt
 
-jmp screen
+jmp ramdump
 
 
 f1left:
@@ -204,12 +231,12 @@ div bl
 cmp ah,0
 je .equal
 dec r15
-jmp screen
+jmp ramdump
     .equal:
     cmp r14,0x800
-    jb screen
+    jb ramdump
     sub r14,0x800
-    jmp screen
+    jmp ramdump
 
 f1right:
 mov rax,r15
@@ -218,42 +245,222 @@ div bl
 cmp ah,0x3f
 je .equal
 inc r15
-jmp screen
+jmp ramdump
     .equal:
     add r14,0x800
-    jmp screen
+    jmp ramdump
 
 f1up:
 cmp r15,0x40
 jb .below
 sub r15,0x40
-jmp screen
+jmp ramdump
     .below:
     cmp r14,0x40
-    jb screen
+    jb ramdump
     sub r14,0x40
-    jmp screen
+    jmp ramdump
 
 f1down:
 cmp r15,0xbbf
 ja .above
 add r15,0x40
-jmp screen
+jmp ramdump
     .above:
     add r14,0x40
-    jmp screen
+    jmp ramdump
 
 f1space:
     mov r14,0x800
     mov r15,0
-    jmp screen
-;___________________________________________________________________
+    jmp ramdump
+
+f1alt:
+inc byte [altkey]
+test byte [altkey],1
+jnz .anscii
+    .hex:
+    mov dword [dumpwhat-4],dumphex-dumpwhat                      ;selfmodify
+    jmp ramdump
+    .anscii:
+    mov dword [dumpwhat-4],dumpanscii-dumpwhat                   ;selfmodify
+    jmp ramdump
+
+;_______________________________________
+altkey db 0
+
+
+
+
+;______________________________
+ramdump:
+push r14
+mov dword [frontcolor],0xff00
+mov dword [linecount],0
+
+dumpline:
+    mov edi,0x100000        ;locate destination
+    mov eax,[linecount]
+    imul eax,4*1024*16
+    add edi,eax
+    call dumphex
+dumpwhat:
+
+inc byte [linecount]
+cmp byte [linecount],0x30
+jb dumpline
+
+pop r14
+
+
+
+
+displayaddress:
+mov dword [frontcolor],0xffffffff
+
+lea rbx,[r14]
+mov edi,0x100000+4*1024*728+4*888
+call dumprbx
+
+lea rbx,[r14+r15]
+mov edi,0x100000+4*1024*744+4*888
+call dumprbx
+
+
+
+
+mouse:
+mov esi,0xb8b0                 ;kuang
+mov edi,0x100000
+;mov edi,[0x3028]
+mov eax,r15d
+mov bl,0x40
+div bl
+
+movzx ebx,ah           ;余数
+imul ebx,32
+shl ebx,1
+add edi,ebx
+
+movzx eax,al           ;商
+imul eax,4*1024*16
+add edi,eax
+
+mov ecx,16
+.first:
+xor edx,edx
+	.second:
+	not dword [edi+edx]
+	add edx,4
+	cmp edx,64
+	jb .second
+add edi,4*1024
+loop .first
+
+call writescreen
+
+jmp forever
+
+;___________________________________
+linecount:dd 0
+
+
+
+
+;_______________________________
+;in:r14=memorystart,edi=destination
+;change:r14+64,rax,rdx,cl
+dumpanscii:
+
+mov ecx,0x40
+.dump:
+    mov dword [frontcolor],0xff00
+    mov al,0x20
+    call char
+
+    mov al,[r14]
+    cmp al,0x20
+    jb .blank
+    cmp al,0x7e
+    jb .visiable
+
+    .blank:
+    mov al,0x20
+
+    .visiable:
+    call char
+    inc r14
+
+dec cl
+jnz .dump
+ret
+;_______________________________
+
+
+
+
+;_______________________________
+;in:r14=memorystart,edi=destination
+;change:r14+64,rax,rdx,cl
+dumphex:
+
+mov cl,0x10
+.dump4:
+mov eax,[r14]
+mov [save32],eax
+add r14,4
+
+  mov ch,4
+  .dump:
+  mov al,[save32]
+  shr al,4
+  call char
+  mov al,[save32]
+  and al,0xf
+  call char
+  shr dword [save32],8
+  dec ch
+  cmp ch,0
+  jne .dump
+
+dec cl
+jnz .dump4
+
+ret
+;_______________________________
+save32:dd 0
+
+
+
+
+;________________________________
+dumprbx:
+mov ecx,16
+.getaddress:
+	rol rbx,4
+	mov al,bl
+	and al,0x0f
+
+	push rbx
+	push rcx
+	call char
+	pop rcx
+	pop rbx
+
+	loop .getaddress
+ret
+;____________________________________
+
+
+
+
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>-----F2----<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 
 
 
 ;_________________________________________
-f2switch:
+function2:
 ;________________________________________
 
 
@@ -268,23 +475,34 @@ mov ecx,1024*768
 cld
 rep movsd
 
-ret
+call writescreen
+
+jmp forever
+
 ;_________________________________________
 jpegbase:dq 0
 
 
 
 
-;___________________________________
-donothing:
-ret
-;_____________________________________
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>-----F3----<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
+
+
+
+
+function3:
+jmp forever
+
+
+
+
+;>>>>>>>>>>>>>>>>>>>>>>>>>>>>>----console----<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
 
 
 
 
 ;_______________________________________________
-consoleswitch:
+function4:
 
 cmp al,0x1c
 je consoleenter
@@ -295,24 +513,24 @@ jmp consoleother
 
 consoleenter:
 call understand
-jmp screen
+jmp console
 
 consolebackspace:
 cmp byte [length],0
-jna screen
+jna console
 
 dec byte [length]
 delete1:
 mov eax,line0
 add eax,[length]
 mov byte [eax],' '
-jmp screen
+jmp console
 
 
 consoleother:
 
 cmp byte [length],64
-jae screen
+jae console
 
 translate:
     mov esi,0xb800                 ;table
@@ -334,7 +552,7 @@ mov ebx,line0
 add ebx,[length]
 mov [ebx],al
 inc byte [length]
-jmp screen
+jmp console
 ;_______________________________________________________________________________
 
 
@@ -388,7 +606,10 @@ cmp byte [looptimes],0x20
 jb .tmd
 mov dword [backcolor],0
 
-ret
+call writescreen
+
+jmp forever
+
 ;________________________________________
 edibackground:dd 0
 ediforeground:dd 0
@@ -498,166 +719,6 @@ ret
 
 
 
-;______________________________
-ramdump:
-push r14
-mov dword [frontcolor],0xff00
-mov dword [linecount],0
-
-dumpline:
-    mov edi,0x100000        ;locate destination
-    mov eax,[linecount]
-    imul eax,4*1024*16
-    add edi,eax
-    call dumphex
-dumpwhat:
-
-inc byte [linecount]
-cmp byte [linecount],0x30
-jb dumpline
-
-pop r14
-
-
-
-
-displayaddress:
-mov dword [frontcolor],0xffffffff
-
-lea rbx,[r14]
-mov edi,0x100000+4*1024*728+4*888
-call dumprbx
-
-lea rbx,[r14+r15]
-mov edi,0x100000+4*1024*744+4*888
-call dumprbx
-
-
-
-
-mouse:
-mov esi,0xb8b0                 ;kuang
-mov edi,0x100000
-;mov edi,[0x3028]
-mov eax,r15d
-mov bl,0x40
-div bl
-
-movzx ebx,ah           ;余数
-imul ebx,32
-shl ebx,1
-add edi,ebx
-
-movzx eax,al           ;商
-imul eax,4*1024*16
-add edi,eax
-
-mov ecx,16
-.first:
-xor edx,edx
-	.second:
-	not dword [edi+edx]
-	add edx,4
-	cmp edx,64
-	jb .second
-add edi,4*1024
-loop .first
-
-ret
-;___________________________________
-linecount:dd 0
-
-
-
-
-;_______________________________
-;in:r14=memorystart,edi=destination
-;change:r14+64,rax,rdx,cl
-dumpanscii:
-
-mov ecx,0x40
-.dump:
-    mov dword [frontcolor],0xff00
-    mov al,0x20
-    call char
-
-    mov al,[r14]
-    cmp al,0x20
-    jb .blank
-    cmp al,0x7e
-    jb .visiable
-
-    .blank:
-    mov al,0x20
-
-    .visiable:
-    call char
-    inc r14
-
-dec cl
-jnz .dump
-ret
-;_______________________________
-
-
-
-
-;_______________________________
-;in:r14=memorystart,edi=destination
-;change:r14+64,rax,rdx,cl
-dumphex:
-
-mov cl,0x10
-.dump4:
-mov eax,[r14]
-mov [save32],eax
-add r14,4
-
-  mov ch,4
-  .dump:
-  mov al,[save32]
-  shr al,4
-  call char
-  mov al,[save32]
-  and al,0xf
-  call char
-  shr dword [save32],8
-  dec ch
-  cmp ch,0
-  jne .dump
-
-dec cl
-jnz .dump4
-
-ret
-;_______________________________
-save32:dd 0
-
-
-
-
-;________________________________
-dumprbx:
-mov ecx,16
-.getaddress:
-	rol rbx,4
-	mov al,bl
-	and al,0x0f
-
-	push rbx
-	push rcx
-	call char
-	pop rcx
-	pop rbx
-
-
-	dec cl
-	jnz .getaddress
-;____________________________________
-
-
-
-
 ;_________________________________
 message:
 mov cl,64
@@ -676,70 +737,3 @@ line0:times 64 db ' '
 line1:times 64 db ' '
 lines:times 64*30 db ' '
 linenull:times 64 db ' '
-
-
-
-
-;进:esi,edi
-;出:esi,edi
-;用:
-;变:
-;______________________________
-char:
-    push rbx
-    push rcx
-
-    movzx eax,al
-    shl ax,4
-    lea esi,[eax+0xb000]
-
-    mov ecx,16           ;16行
-.yihang:
-    xor rax,rax
-    lodsb
-    push rcx
-        mov cl,8
-      .yidian:
-        shl al,1
-        jnc .bg
-        mov edx,[frontcolor]
-        mov [edi],edx
-        jmp .pointdone
-      .bg:
-        mov edx,[backcolor]
-        cmp edx,0xffffffff
-        je .pointdone
-        mov dword [edi],edx
-      .pointdone:
-        add edi,4
-        loop .yidian
-    sub edi,32           ;每个字的行头
-    add edi,1024*4            ;下1行
-    pop rcx
-    loop .yihang
-
-    add edi,32            ;下个字的行头
-    sub edi,4*1024*16            ;上16行;现在edi=下个字开头
-
-    mov eax,edi
-    mov ebx,0x100000        ;ebx=vesabase
-    ;mov ebx,[0x3028]        ;ebx=vesabase
-    sub eax,ebx             ;eax=相对距离
-
-.modfour:
-    and eax,0x00000fff           ;mod4096
-    cmp eax,0
-    jne .nochange
-
-.change:
-    add edi,4*1024*16
-    sub edi,1024*4
-
-.nochange:
-    pop rcx
-    pop rbx
-
-    ret              ;edi指向下一个字
-;______________________________________
-backcolor:dd 0
-frontcolor:dd 0xffffffff
