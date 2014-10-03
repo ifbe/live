d66d8cf919e060e5f2c876b171871d0c91a65b40
ifbe <ifbe@live.cn>
2014-10-03 20:35:31 +0800

    modified:   init/cpu/4th/apic.s 	modified:   init/jarvis/init/acpi.s 	modified:   init/jarvis/init/pciold.s

--- init/cpu/4th/apic.s
+++ init/cpu/4th/apic.s
@@ -16,36 +16,15 @@ findlocalapic:
     ;bts eax,11            ;enable lapic
     ;wrmsr
     and eax,0xfffff000
-    mov [rel localapicaddress],eax    ;save lapic base
-
     cmp eax,0xfee00000
     jb endoflocalapic
 ;_____________________________________
 
 
-;___________________________________
-savelapic:
-    mov edi,0x4000
-.continue:
-    cmp dword [edi],0
-    jne .addandagain
-	mov rax,"lapic"
-	stosq
-	mov eax,[rel localapicaddress]
-	stosq
-	jmp .endofthis
-.addandagain:
-    add edi,0x10
-    cmp edi,0x5000
-    jb .continue
-;___________________________________
-.endofthis:
-
-
 ;________________________________________
 localapicinitialization:
 
-    mov edi,[rel localapicaddress]
+    mov edi,0xfee00000
 
     ;[+80]=0              (task priority=0)
     xor eax,eax
@@ -76,12 +55,6 @@ localapicinitialization:
     mov [edi+0xf0],eax
 
 ;_________________________________________
-
-    jmp endoflocalapic
-
-
-localapicaddress:dq 0
-
 endoflocalapic:
 
 
@@ -92,86 +65,8 @@ endoflocalapic:
 
 
 startofioapic:
-;_______________________________________
-searchioapic:
-    mov edi,0x4000
-.search:
-    cmp dword [edi],"MADT"
-    je findioapic
-    cmp dword [edi],"APIC"
-    je findioapic
-    add edi,0x10
-    cmp edi,0x5000
-    jb .search
-;_______________________________________-
-
-    jmp endofioapic
-
-
-;_________________________________
-findioapic:
-
-getinformation:
-    mov edi,[edi+8]
-    mov [rel madtstart],edi
-    mov eax,[edi+4]
-    add eax,edi
-    mov [rel madtend],eax
-    add edi,0x2c
-    mov [rel tablestart],edi
-
-analyse:
-    cmp byte [edi],1
-    jne .continue
-
-	mov eax,[edi+4]
-	mov [rel ioapicaddress],eax
-	cmp eax,0xfec00000
-	jne endofioapic
-	jmp saveioapic            ;find it,lets go
-
-.continue:
-    mov al,[edi+1]
-    movzx eax,al
-    add edi,eax
-    cmp edi,[rel madtend]
-    jb analyse
-;_____________________________
-
-    jmp endofioapic             ;cannot find
-
-
-;________________________________________
-madtstart:dq 0
-madtend:dq 0
-tablestart:dq 0
-ioapicaddress:dq 0
-;___________________________________
-
-
-;___________________________________
-saveioapic:
-    mov edi,0x4000
-.continue:
-    cmp dword [edi],0
-    jne .addandagain
-
-	mov rax,"ioapic"
-	stosq
-	mov eax,[rel ioapicaddress]
-	stosq
-	jmp .endofthis
-
-.addandagain:
-    add edi,0x10
-    cmp edi,0x5000
-    jb .continue
-;___________________________________
-.endofthis:
-
-
 ;____________________________________
-ioapicinitialization:
+initioapic:
 
 disable8259:
     mov al, 0xff
@@ -196,6 +91,12 @@ loop maskall
 
 
 jmp endofioapic
+
+
+madtstart:dq 0
+madtend:dq 0
+tablestart:dq 0
+ioapicaddress:dq 0
 
 
 paddingofioapic:
--- init/jarvis/init/acpi.s
+++ init/jarvis/init/acpi.s
@@ -1,43 +1,51 @@
-startofacpi:
+;/acpi
+;如果要改位置,搜索0x100000和0x101000
+
+
 [BITS 64]
-;进:
-;出:rbp
-;用:
-;变:rax,rbp,cx,*0x4008=RSD PTR 
+
+
+startofacpi:
+;______________________clear____________________________
+mov edi,0x100000
+mov ecx,0x2000
+xor rax,rax
+rep stosq
+;____________________________________________________
+
+
+
+
 ;_________look for rsd ptr from e0000~ffff0__________
     mov rax,"RSD PTR "
-    mov edi,0x4000
+    mov edi,0x100000
     stosq
     mov rbp,0xe0000
-    mov cx,0x2000
+    mov ecx,0x2000
 rsdptr:
     cmp [rbp],rax
-    je findrsdptr
+    je findrsdptr		;找到的地址在rbp
     add rbp,0x10
     loop rsdptr
 
-    qusiba: hlt             ;if not find
-    jmp qusiba
+    jmp endofacpi
 ;_____________________________________________
 
 
-;进:rbp
-;出:
-;用:
-;变:*0x4000=&rsdp
-;____________________________________
+
+
+;________________rsdt/xsdt?_________________
 findrsdptr:
     mov rax,rbp       ;rsdptr
     stosq
     cmp byte [rbp+0xf],0x00 ;不是0就是xsdt
     jne xsdt
+;_________________________________________
 
 
-;进:rbp
-;出:
-;用:
-;变:eax,rbp,cx,*0x4018="RSDT",*0x4010=&rsdt
-;_______________________________
+
+
+;____________________________________
 rsdt:
     mov eax,"RSDT"
     stosq
@@ -46,8 +54,7 @@ rsdt:
     mov cl,[eax+4]
     sub cl,0x24
     add eax,0x24
-    mov edi,0x4020
-.rsdttable:
+.rsdttable:			;把每个表翻译到指定地方（di）
     mov esi,[eax]
     mov [edi+8],esi
     movsd
@@ -61,10 +68,8 @@ rsdt:
 ;__________________________________
 
 
-;进:rbp
-;出:
-;用:
-;变:rax,rbp,cx,*0x4018="RSDT",*0x4010=&rsdt
+
+
 ;____________________________________
 xsdt:
     mov eax,"XSDT"
@@ -74,8 +79,7 @@ xsdt:
     mov cl,[rax+4]
     sub cl,0x24
     add eax,0x24
-    mov edi,0x4020
-.xsdttable:
+.xsdttable:			;把每个表翻译到指定地方（di）
     mov esi,[eax]
     mov [edi+0x8],esi
     movsq
@@ -87,56 +91,146 @@ xsdt:
 ;_____________________________________________
 
 
-;进:
-;出:
-;用:
-;变:rax,edi,ecx，*0xb8000~*0xbffff
+
+
+;_______________处理每一项______________________
 acpi:
-;_______________search in facp for port________________________________
-    mov esi,0x4020            ;8008和8018分别是rsdp和xsdt
+    mov dword [rel saveaddr],0x100000
+
 .search:
-    cmp dword [esi],"FACP"
-    je facp
-    add si,0x10
-    cmp esi,0x4400
+    mov esi,[rel saveaddr]	;switch(名字)
+
+    cmp dword [esi],"FACP"	;case facp
+    jne .notfacp
+    call explainfacp
+    .notfacp:
+
+    cmp dword [esi],"APIC"	;case apic
+    jne .notapic
+    call explainapic
+    .notapic:
+
+    cmp dword [esi],"MADT"	;case madt
+    jne .notmadt
+    call explainapic
+    .notmadt:
+
+    cmp dword [esi],"what"	;case else....
+    jne .notwhat
+    call explainelse
+    .notwhat:
+
+    add dword [rel saveaddr],0x10
+    cmp dword [rel saveaddr],0x101000
     jb .search
-facp:
-    mov esi,[esi+0x8]           ;[esi-0x8]=facp
-    mov ax,[esi+0x40]           ;[facp+0x40]=port
-    mov [0x4fc],ax
-    mov eax,[esi+0x28]
-    mov [edi+8],eax
-    mov eax,[eax]
-    mov [edi],eax
-
-    mov esi,[edi+0x8]
-    mov eax,"_S5_"
-dsdt:
-    cmp [esi],eax
-    je find_S5_
-    inc esi
-    jmp dsdt
-;_________________________________________
-
-
-
-
-find_S5_:
-;_________________________________
-    xor ah,ah
-    mov al,[esi+8]           ;esi=_S5_
-    mov cl,10
-    shl ax,cl
-    mov [0x4fe],ax
-
-    ;below is how to shutdown
-    ;mov dx,[0x4fc]    mov ax,[0x4fe]    or ax,0x2000    out dx,ax
-;_________________________________________
 
     jmp endofacpi
+;_________________________________________________
+saveaddr:dq 0
 
-paddingofacpi:
-times 0x100-(paddingofacpi-startofacpi) db 0
+
+
+
+;[esi]:名字
+;[esi+8]:地址
+;edi:往哪儿写
+;_________________________________________________
+explainfacp:
+    mov esi,[esi+0x8]           ;facpaddr=[esi-0x8]
+
+    mov eax,[esi+0x28]		;dsdtaddr=[facpaddr+0x28]
+    mov [edi+8],eax
+    mov eax,[eax]		;dsdt=[dsdtaddr]
+    mov [edi],eax
+    add edi,0x10
+
+    mov edx,[edi-8]		;dsdtaddr
+    mov eax,"_S5_"
+    stosq
+searchs5:			;dsdt
+    cmp [edx],eax
+    jne .nots5
+	mov eax,edx
+	stosq
+	jmp finds5
+    .nots5:
+    inc edx
+    jmp searchs5		;we must find it
+    ;cmp edx,0x100000
+    ;jb searchs5
+    ;ret
+
+finds5:
+    mov eax,"data"
+    stosq
+    xor rax,rax
+    mov al,[edx+8]		;edx=_s5_addr
+    shl ax,10
+    stosq
+    mov [0x4fe],ax
+
+    mov eax,"port"
+    stosq
+    xor rax,rax
+    mov ax,[esi+0x40]           ;data=[facpaddr+0x40]
+    stosq
+    mov [0x4fc],ax
+
+    ret
+
+
+    ;how to shutdown:
+    ;mov dx,port
+    ;mov ax,data
+    ;or ax,0x2000
+    ;out dx,ax
+;_________________________________________
+
+
+
+
+;[esi]:名字
+;[esi+8]:地址
+;edi:往哪儿写
+;_________________________________
+explainapic:
+    mov esi,[esi+8]			;madtaddr
+
+    mov rax,"table"
+    stosq
+    lea eax,[esi+0x2c]
+    stosq
+
+    mov edx,eax				;madtstart
+    mov ecx,[esi+4]
+    add ecx,esi				;madtend
+analyse:
+    cmp byte [edx],1
+    jne .continue
+
+    mov rax,"ioapic"
+    stosq
+    mov eax,[edx+4]
+    stosq
+    ret
+
+.continue:
+    mov al,[edx+1]
+    movzx eax,al
+    add edx,eax
+    cmp edx,ecx
+    jb analyse
+
+    ret
+;_____________________________
+
+
+
+
+explainelse:
+ret
+
+
 
 
 endofacpi:
