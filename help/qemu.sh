717c3d3be5f2ce6ceab51664bc9ff5958141a92e
ifbe <ifbe@live.cn>
2014-10-28 08:24:29 +0800

    modified:   help/qemu.sh

--- help/qemu.sh
+++ help/qemu.sh
@@ -10,11 +10,13 @@ sudo qemu-kvm \
 -monitor stdio \
 -smp 2 \
 -m 512 \
--device usb-ehci,id=ehci \
--device nec-usb-xhci,id=xhci \
--device usb-tablet,bus=xhci.0,port=2 \
--device usb-host,vendorid=0x0eef,productid=0x1,bus=xhci.0,port=3 \
--device usb-host,vendorid=0x067b,productid=0x2303,bus=xhci.0,port=4 \
 -device ahci,id=ahci \
 -device ide-drive,drive=disk,bus=ahci.0 \
--drive id=disk,if=none,file=$1
+-drive id=disk,if=none,file=$1 \
+-device usb-ehci,id=ehci \
+-device nec-usb-xhci,id=xhci \
+-device usb-tablet,bus=xhci.0,port=1 \
+-device usb-hub,id=hub1,bus=xhci.0,port=2 \
+-device usb-host,vendorid=0x0eef,productid=0x1,bus=xhci.0,port=3 \
+-device usb-host,vendorid=0x067b,productid=0x2303,bus=xhci.0,port=4 \
+-device usb-net,bus=xhci.0,port=2.1
