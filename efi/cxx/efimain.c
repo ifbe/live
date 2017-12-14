73c47533bbf18fa571556b50c03d7c3dc23501d6
ifbe <ifbe@live.cn>
2017-12-14 14:04:29 +0800

    fix

---
+++ efi/cxx/efimain.c
@@ -0,0 +1,25 @@
+#include <efi.h>
+#include <efilib.h>
+void main();
+
+
+
+ 
+EFI_STATUS efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable)
+{
+	EFI_STATUS Status;
+	EFI_INPUT_KEY Key;
+	ST = SystemTable;
+ 
+	Status = ST->ConOut->OutputString(ST->ConOut, L"Hello World\n\r");
+	if (EFI_ERROR(Status))return Status;
+ 
+	Status = ST->ConIn->Reset(ST->ConIn, FALSE);
+	if (EFI_ERROR(Status))return Status;
+ 
+	main();
+
+	while ((Status = ST->ConIn->ReadKeyStroke(ST->ConIn, &Key)) == EFI_NOT_READY) ;
+	return Status;
+}
+void atexit(){}
