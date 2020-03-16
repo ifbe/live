25c01af0e45b75cc28c8b164a12dbb3fa38da43f
ifbe <ifbe@live.cn>
2020-03-16 22:47:30 +0800

    fix x64: enable serial only if 0x3f8==[0x400,0x401]

--- kernel/libhard0/x64/uart/8250.c
+++ kernel/libhard0/x64/uart/8250.c
@@ -2,22 +2,31 @@
 #define u16 unsigned short
 #define u32 unsigned int
 #define u64 unsigned long long
-#define PORT 0x3f8
+#define BIOSDATA 0x400
+#define UARTPORT 0x3f8
 void out8(u32 port, u8 data);
 u8 in8(u32 port);
 
 
 
 
+static int enable = 0;
+
+
+
+
 int readuart_one(u8* buf)
 {
-	if(0 == (in8(PORT+5) & 1))return 0xffff;
+	if(UARTPORT != enable)return 0xffff;
 
-	return in8(PORT);
+	if(0 == (in8(UARTPORT+5) & 1))return 0xffff;
+	return in8(UARTPORT);
 }
 int readuart(u8* buf, int len)
 {
 	int j,ret;
+	if(UARTPORT != enable)return 0;
+
 	for(j=0;j<len;j++)
 	{
 		ret = readuart_one(buf+j);
@@ -34,17 +43,21 @@ int readuart(u8* buf, int len)
 int writeuart_one(u8 data)
 {
 	int j=0;
-	while((in8(PORT + 5) & 0x20) == 0)
+	if(UARTPORT != enable)return 0xffff;
+
+	while((in8(UARTPORT + 5) & 0x20) == 0)
 	{
 		j++;
 		if(j>0xffffff)return 0;
 	}
-	out8(PORT, data);
+	out8(UARTPORT, data);
 	return 1;
 }
 int writeuart(u8* buf, int len)
 {
 	int j;
+	if(UARTPORT != enable)return 0;
+
 	for(j=0;j<len;j++)writeuart_one(buf[j]);
 	return j;
 }
@@ -54,13 +67,16 @@ int writeuart(u8* buf, int len)
 
 void inituart()
 {
-	out8(PORT + 1, 0x00);//Disable all interrupts
-	out8(PORT + 3, 0x80);//Enable DLAB (set baud rate divisor)
-	out8(PORT + 0, 0x01);//1=115200, 3=38400
-	out8(PORT + 1, 0x00);//(high byte)
-	out8(PORT + 3, 0x03);//8 bits, no parity, one stop bit
-	out8(PORT + 2, 0xC7);//Enable FIFO, clear them, with 14-byte threshold
-	out8(PORT + 4, 0x0B);//IRQs enabled, RTS/DSR set
+	if(UARTPORT != *(u16*)(BIOSDATA+0))return;
+
+	out8(UARTPORT + 1, 0x00);//Disable all interrupts
+	out8(UARTPORT + 3, 0x80);//Enable DLAB (set baud rate divisor)
+	out8(UARTPORT + 0, 0x01);//1=115200, 3=38400
+	out8(UARTPORT + 1, 0x00);//(high byte)
+	out8(UARTPORT + 3, 0x03);//8 bits, no parity, one stop bit
+	out8(UARTPORT + 2, 0xC7);//Enable FIFO, clear them, with 14-byte threshold
+	out8(UARTPORT + 4, 0x0B);//IRQs enabled, RTS/DSR set
 
 	writeuart((u8*)"fuckyou\n", 8);
+	enable = UARTPORT;
 }
